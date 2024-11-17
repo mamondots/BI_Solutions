19b116900db67946098243599422b694d68ed8af
Al Mamon <shikderr112@gmail.com>
2024-11-17 18:33:28 +0600

    basename

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -3,19 +3,24 @@ import Main from "../Layout/Main";
 import LandingPage from "../Landing_page/LandingPage";
 import Blog from "../Blog/Blog";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <LandingPage></LandingPage>,
+        },
+      ],
+    },
+    {
+      path: "/blog",
+      element: <Blog></Blog>,
+    },
+  ],
   {
-    path: "/",
-    element: <Main></Main>,
-    children: [
-      {
-        path: "/",
-        element: <LandingPage></LandingPage>,
-      },
-    ],
-  },
-  {
-    path: "/blog",
-    element: <Blog></Blog>,
-  },
-]);
+    basename: "/BI_Solutions",
+  }
+);
